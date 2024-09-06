648f1cf334bc79f0d3fed2998eafce2b8d892803
Hermesgb <hermesgabriel@gmail.com>
2024-09-06 12:08:41 +0200

    Fecha y hora actuales salen junto al nombre al hacer clic.

--- static/main.js
+++ static/main.js
@@ -1,13 +1,22 @@
 const obtenerTextoBoton = document.getElementById('obtenerTextoBoton');
 const resultadoDiv = document.getElementById('resultado');
+const currentDate = document.getElementById('fecha');
+const currentTime = document.getElementById('hora');
 
-obtenerTextoBoton.addEventListener('click', () => {
-  fetch('http://localhost:3000/texto')
-    .then(response => response.json())
-    .then(data => {
-      resultadoDiv.textContent = data.nombre;
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
-});
+function onObtenerTexto(event) {
+    const ahora = new Date();
+    // currentDate.innerHTML = currentTime.innerHTML = ahora.toLocaleDateString("es-ES");
+    currentDate.innerHTML = ahora.toLocaleDateString("es-ES");
+    currentTime.innerHTML = ahora.toLocaleTimeString("es-ES");
+    
+    fetch('http://localhost:3000/texto')
+        .then(response => response.json())
+        .then(data => {
+            resultadoDiv.textContent = data.nombre;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+
+obtenerTextoBoton.addEventListener('click', onObtenerTexto);
