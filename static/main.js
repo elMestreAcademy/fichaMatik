57c989414c48a4604551a5616c52585ae935b61a
Muscardina <vergonzalezsainz@gmail.com>
2024-09-26 11:49:02 +0200

    El front se comunica con la base de datos para devolver el nombre del trabajador

--- static/main.js
+++ static/main.js
@@ -3,82 +3,9 @@ const nombre = document.getElementById("nombre");
 const primer_apellido = document.getElementById("primer-apellido");
 const segundo_apellido = document.getElementById("segundo-apellido");
 const currentDate = document.getElementById("fecha");
-const currentTime = document.getElementById("hora");
-//const salida = document.getElementById("salida");
+const horaRecuperada = document.getElementById('hora-recuperada');
 
 const server = "http://localhost:3000";
-/*
-function showCurrendatetime() {
-    const ahora = new Date();
-    // currentDate.innerHTML = currentTime.innerHTML = ahora.toLocaleDateString('es-ES');
-    currentDate.innerHTML = ahora.toLocaleDateString('es-ES');
-    currentTime.innerHTML = ahora.toLocaleTimeString('es-ES');
-}
-
-function onObtenerTexto(event) {
-    showCurrendatetime()
-
-    fetch(`${server}/texto`,
-        {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ usuario_id: 1 })
-        }
-    )
-        .then(response => response.json())
-        .then(data => {
-            nombre.textContent = data.nombre;
-            primer_apellido.textContent = data.primer_apellido;
-            segundo_apellido.textContent = data.segundo_apellido;
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-}
-
-function guardarDatos(event) {
-    showCurrendatetime()
-
-    fetch(`${server}/fichar`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ usuario_id: 1 })
-    })
-        .then(response => response.text())
-        .then(data => {
-            console.log(data);
-            // Mostrar un mensaje al usuario indicando si se guardaron los datos correctamente
-        })
-        .catch(error => {
-            console.error(error);
-        });
-}
-
-function onSalida(event) {
-    showCurrendatetime()
-
-    fetch(`${server}/salida`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ usuario_id: 1 })
-    })
-        .then(response => response.text())
-        .then(data => {
-            console.log(data);
-            // Mostrar un mensaje al usuario indicando si se guardaron los datos correctamente
-        })
-        .catch(error => {
-            console.error(error);
-        });
-}
-
-*/
 
 function onObtenerTexto(event) {
   const usuario_id = document.getElementById("usuario_id").value;
@@ -95,7 +22,7 @@ function onObtenerTexto(event) {
       resultadoDiv.innerHTML = `
         <div>${
           data.fichaje_id
-            ? "El usuario a ingresado a las " + data.entrada + ". ¿Desea salir?"
+            ? data.nombre + " " + data.primer_apellido + " " + data.segundo_apellido + " ha ingresado a las " + data.entrada + ". ¿Desea salir?"
             : "EL usuario no tiene un fichaje activo. ¿Desea entrar?"
         }</div>
         <button id='fichaje'>${data.fichaje_id ? "Salir" : "Fichar"}</button>
@@ -151,7 +78,7 @@ function salida() {
     .then((data) => {
       const resultadoDiv = document.getElementById("resultado");
       resultadoDiv.innerHTML = `
-            <div>El usuario ha salido correctametne</div>
+            <div>El usuario ha salido correctamente</div>
           `;
     })
     .catch((error) => {
@@ -160,5 +87,4 @@ function salida() {
 }
 
 obtenerTextoBoton.addEventListener("click", onObtenerTexto);
-// obtenerTextoBoton.addEventListener("click", guardarDatos);
-// obtenerTextoBoton.addEventListener("click", onSalida);
+
